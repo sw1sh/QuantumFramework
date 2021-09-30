f87fda20ee95f28b74dd9377ffbcf625f9ad5f62
Nikolay Murzin <nikm@wolfram.com>
2021-10-01 01:19:59 +0300

    add documentation notebooks

---
+++ PacletInfo.m
@@ -0,0 +1,29 @@
+PacletObject[
+  <|
+    "Name" -> "QuantumFramework",
+    "Description" -> "Quantum Computing",
+    "Creator" -> "nikm",
+    "Version" -> "0.2",
+    "WolframVersion" -> "13+",
+    "Extensions" -> {
+      {
+        "Kernel",
+        "Root" -> "QuantumFramework",
+        "Context" -> "QuantumFramework`",
+        "Symbols" -> {
+            "QuantumFramework`QuantumBasisName",
+            "QuantumFramework`QuditBasis",
+            "QuantumFramework`QuantumBasis",
+            "QuantumFramework`QuantumState",
+            "QuantumFramework`QuantumOperator",
+            "QuantumFramework`QuantumMeasurementOperator",
+            "QuantumFramework`QuantumCircuitOperator",
+            "QuantumFramework`QuantumMeasurement",
+            "QuantumFramework`QuantumTensorProduct",
+            "QuantumFramework`QuantumPartialTrace"
+        }
+      },
+      {"Documentation", "Language" -> "English"}
+    }
+  |>
+]
