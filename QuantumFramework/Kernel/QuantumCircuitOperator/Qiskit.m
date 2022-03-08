ff3d38e33021e238dd24d7c967521ced2776f9a0
Nikolay Murzin <nikm@wolfram.com>
2022-03-08 03:27:40 +0300

    add qiskit simulation prototype

--- QuantumFramework/Kernel/QuantumCircuitOperator/Qiskit.m
+++ QuantumFramework/Kernel/QuantumCircuitOperator/Qiskit.m
@@ -6,26 +6,31 @@ PackageExport["QuantumCircuitOperatorToQiskit"]
 
 
 existingGateNames = {
-    "barrier", "measure", "reset", "u3", "u2", "u1", 
-    "cx", "id", "u0", "u", "p", "x", "y", "z", "h", "s", "sdg", "t", 
-    "tdg", "rx", "ry", "rz", "sx", "sxdg", "cz", "cy", "swap", "ch", 
-    "ccx", "cswap", "crx", "cry", "crz", "cu1", "cp", "cu3", "csx", 
+    "barrier", "measure", "reset", "u3", "u2", "u1",
+    "cx", "id", "u0", "u", "p", "x", "y", "z", "h", "s", "sdg", "t",
+    "tdg", "rx", "ry", "rz", "sx", "sxdg", "cz", "cy", "swap", "ch",
+    "ccx", "cswap", "crx", "cry", "crz", "cu1", "cp", "cu3", "csx",
     "cu", "rxx", "rzz", "rccx", "rc3x", "c3x", "c3sx", "c4x"
 };
 
 labelToGate = Replace[{
     "Controlled"[x_String, ___] :> "c" <> ToLowerCase[x],
-    "Computational" | "Computational"[___] -> "measure",
+    Superscript[x_String, CircleTimes[_]] :> x,
     x_String :> ToLowerCase[x]
 }]
 
 QuantumCircuitOperatorToQiskit[qco_QuantumCircuitOperator] := Enclose @ Block[{
     operators = Map[
         With[{
-            label = labelToGate @ #["Label"]
+            label = If[QuantumMeasurementOperatorQ[#], "measure", labelToGate @ #["Label"]]
         },
-            If[ label == "measure",
+            Switch[
+                label,
+                "measure",
                 Splice[{label, None, {#, #}} & /@ (#["InputOrder"] - 1)],
+                "c",
+                {label, First @ #["ControlOrder"], First @ #["TargetOrder"]},
+                _,
                 {
                     label,
                     NumericArray @ N @ Normal @ #["MatrixRepresentation"],
@@ -35,7 +40,7 @@ QuantumCircuitOperatorToQiskit[qco_QuantumCircuitOperator] := Enclose @ Block[{
         ] &,
         qco["Operators"]
     ],
-    arity = {qco["Arity"], Replace[Quiet[qco["Eigenqudits"]], Except[_Integer] -> Nothing]}
+    arity = {qco["Arity"], Replace[Quiet[qco["Targets"]], Except[_Integer] -> Nothing]}
 },
     ExternalEvaluate[Confirm @ $PythonSession, "
 from wolframclient.language import wl
@@ -92,22 +97,54 @@ qc_QiskitCircuit["Depth"] := qc["Eval", "depth"]
 qc_QiskitCircuit["Ops"] := qc["Eval", "count_ops"]
 
 
-qiskitMatrix[qc_QiskitCircuit] := Block[{Wolfram`QuantumFramework`pythonBytes = qc["Bytes"]},
+qiskitMatrix[qc_QiskitCircuit] := Block[{Wolfram`QuantumFramework`$pythonBytes = qc["Bytes"]},
     ExternalEvaluate[$PythonSession, "
 from qiskit import BasicAer, transpile
 
 import pickle
 
-qc = pickle.loads(<* Wolfram`QuantumFramework`pythonBytes *>)
+qc = pickle.loads(<* Wolfram`QuantumFramework`$pythonBytes *>)
 
 backend = BasicAer.get_backend('unitary_simulator')
 
 job = backend.run(transpile(qc, backend))
 job.result().get_unitary(qc, decimals=3)
-    "]
+"]
 ]
 
+Options[qiskitApply] = {"Backend" -> "ibmq_qasm_simulator", "OptimizationLevel" -> 3}
+
+qiskitApply[qc_QiskitCircuit, qs_QuantumState, OptionsPattern[]] := Enclose @ Block[{
+    Wolfram`QuantumFramework`$pythonBytes = qc["Bytes"],
+    Wolfram`QuantumFramework`$qubits = qs["OutputQudits"],
+    Wolfram`QuantumFramework`$state = NumericArray @ Normal @ N @ qs["StateVector"],
+    Wolfram`QuantumFramework`$opts = <|"backend_name" -> OptionValue["Backend"], "optimization_level" -> OptionValue["OptimizationLevel"]|>
+},
+    ConfirmAssert[qs["InputDimensions"] == {1}];
+    ConfirmAssert[AllTrue[qs["OutputDimensions"], EqualTo[2]]];
+    ExternalEvaluate[$PythonSession, "
+import pickle
+from qiskit import IBMQ, QuantumCircuit
+
+qc = pickle.loads(<* Wolfram`QuantumFramework`$pythonBytes *>)
+
+qubits = <* Wolfram`QuantumFramework`$qubits *>
+circuit = QuantumCircuit(qubits, qubits)
+circuit.initialize(<* Wolfram`QuantumFramework`$state *>)
+circuit.extend(qc)
+
+if not IBMQ._credentials:
+    provider = IBMQ.load_account()
+else:
+    provider = IBMQ.get_provider()
+provider.run_circuits(circuit, ** <* Wolfram`QuantumFramework`$opts *>).result().get_counts()
+"]
+]
+
+
 qc_QiskitCircuit["Matrix"] := qiskitMatrix[qc]
+
+qc_QiskitCircuit[qs_QuantumState, opts : OptionsPattern[qiskitApply]] := qiskitApply[qc, qs, opts]
 
 
 (* Formatting *)
