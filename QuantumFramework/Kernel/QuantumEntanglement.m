0ea15027ddd6fb8c8b4e5fa258dcfa2b1b138644
Nikolay Murzin <nikm@wolfram.com>
2022-05-01 07:29:17 +0300

    enclose QuantumEntanglement

--- QuantumFramework/Kernel/QuantumEntanglement.m
+++ QuantumFramework/Kernel/QuantumEntanglement.m
@@ -13,11 +13,11 @@ QuantumEntanglementMonotone[qs_ ? QuantumStateQ, biPartition : Except[_String] :
     QuantumEntanglementMonotone[qs, biPartition, "Concurrence"]
 
 
-QuantumEntanglementMonotone[qs_ ? QuantumStateQ, biPartition_ : Automatic, "Concurrence"] := Module[{
+QuantumEntanglementMonotone[qs_ ? QuantumStateQ, biPartition_ : Automatic, "Concurrence"] := Enclose @ Module[{
     s, ds, twoQuditPauliY
 },
 
-    s = qs["Bipartition", biPartition];
+    s = ConfirmBy[qs["Bipartition", biPartition], QuantumStateQ];
     ds = s["Dimensions"];
     twoQuditPauliY = QuantumTensorProduct[QuantumOperator[{"Y", ds[[1]]}], QuantumOperator[{"Y", ds[[2]]}]];
 
@@ -26,20 +26,26 @@ QuantumEntanglementMonotone[qs_ ? QuantumStateQ, biPartition_ : Automatic, "Conc
 
 
 QuantumEntanglementMonotone[qs_ ? QuantumStateQ, biPartition_ : Automatic, "Negativity"] :=
-    (2 ^ QuantumEntanglementMonotone[qs["Bipartition", biPartition], "LogNegativity"] - 1) / 2
+    Enclose[(2 ^ QuantumEntanglementMonotone[ConfirmBy[qs["Bipartition", biPartition], QuantumStateQ], "LogNegativity"] - 1) / 2]
 
 
 QuantumEntanglementMonotone[qs_ ? QuantumStateQ, biPartition_ : Automatic, "LogNegativity"] :=
-    Chop @ Log[2, Total @ SingularValueList @ QuantumPartialTrace[qs["Bipartition", biPartition], {1}]["NormalizedDensityMatrix"]]
+    Enclose[Chop @ Log[2, Total @ SingularValueList @ QuantumPartialTrace[ConfirmBy[qs["Bipartition", biPartition], QuantumStateQ], {1}]["NormalizedDensityMatrix"]]]
 
 
 QuantumEntanglementMonotone[qs_ ? QuantumStateQ, biPartition_ : Automatic, "EntanglementEntropy"] :=
-    QuantumPartialTrace[qs["Bipartition", biPartition], {1}]["VonNeumannEntropy"]
+    Enclose @ QuantumPartialTrace[ConfirmBy[qs["Bipartition", biPartition], QuantumStateQ], {1}]["VonNeumannEntropy"]
 
 
 QuantumEntanglementMonotone[qs_ ? QuantumStateQ, biPartition_ : Automatic, "RenyiEntanglementEntropy"] :=
     QuantumEntanglementMonotone[qs, biPartition, {"RenyiEntanglementEntropy", 1 / 2}]
 
 QuantumEntanglementMonotone[qs_ ? QuantumStateQ, biPartition_ : Automatic, {"RenyiEntanglementEntropy", alpha_}] :=
-    (1 / (1 - alpha)) Log[2, Total[Select[Re[Eigenvalues[MatrixPower[QuantumPartialTrace[qs["Bipartition", biPartition], {1}]["NormalizedDensityMatrix"], alpha]]], # > 0 &]]]
+    Enclose[
+        (1 / (1 - alpha)) Log[2, Total[Select[Re[Eigenvalues[MatrixPower[
+            QuantumPartialTrace[
+                ConfirmBy[qs["Bipartition", biPartition], QuantumStateQ],
+                {1}
+            ]["NormalizedDensityMatrix"], alpha]]], # > 0 &]]]
+    ]
 
