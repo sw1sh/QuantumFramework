c0e01fd87622a0f469a385113911dcbd724df0c3
Nikolay Murzin <nikm@wolfram.com>
2022-10-29 02:50:14 +0300

    add Multiplexer circuit

--- QuantumFramework/Kernel/QuantumCircuitOperator/NamedCircuits.m
+++ QuantumFramework/Kernel/QuantumCircuitOperator/NamedCircuits.m
@@ -18,7 +18,8 @@ $QuantumCircuitOperatorNames = {
     "PhaseEstimation",
     "Controlled",
     "Trotterization",
-    "Magic"
+    "Magic",
+    "Multiplexer"
 }
 
 
@@ -443,4 +444,10 @@ QuantumCircuitOperator[{"Trotterization", opArgs_, args___}] := Block[{
 ]
 
 QuantumCircuitOperator["Magic"] := QuantumCircuitOperator[{"S" -> {1, 2}, "H" -> {2}, "CNOT" -> {2, 1}}]
+
+
+QuantumCircuitOperator[{"Multiplexer", ops__}] := Block[{n = Length[{ops}], seq},
+    seq = Values[<|0 -> {}, 1 -> {}, PositionIndex[#]|>] & /@ Take[Tuples[{1, 0}, Ceiling[Log2[n]]], n];
+    QuantumCircuitOperator[MapThread[{"C", #1, Splice[#2]} &, {{ops}, seq}]]
+]
 
