c714a93a04691292932554a3afb73a776cff805a
Nikolay Murzin <nikm@wolfram.com>
2023-02-14 20:01:55 +0300

    add QuantumLabelName function

--- QuantumFramework/Kernel/Labels.m
+++ QuantumFramework/Kernel/Labels.m
@@ -1,5 +1,7 @@
 Package["Wolfram`QuantumFramework`"]
 
+PackageExport["QuantumLabelName"]
+
 PackageScope["simplifyLabel"]
 
 
@@ -18,5 +20,24 @@ simplifyLabel[l_] := Replace[l, {
     SuperDagger["U2"[a_, b_]] :> "U2"[Pi - a, Pi - b],
     SuperDagger["\[Pi]"[args__]] :> "\[Pi]"[args],
     SuperDagger[SuperDagger[label_]] :> label
+}]
+
+
+QuantumLabelName[qo_QuantumOperator] := QuantumLabelName[qo["Label"], qo["TargetOrder"]]
+
+QuantumLabelName[qmo_QuantumMeasurementOperator] := qmo["InputOrder"]
+
+QuantumLabelName[qc_QuantumCircuitOperator] := QuantumLabelName /@ qc["Operators"]
+
+QuantumLabelName[label_, order_ : {}] := Replace[label, {
+	Subscript["C", subLabel_Composition][c0_, c1_] :> ({"C", QuantumLabelName[#] -> order, c0, c1} & /@ Reverse[List @@ subLabel]),
+	Subscript["C", subLabel_][c0_, c1_] :> {"C", QuantumLabelName[subLabel, order], c0, c1},
+	HoldPattern[Composition[subLabels___]] :> Reverse[QuantumLabelName /@ {subLabels}] -> order,
+	Superscript[subLabel_, CircleTimes[n_Integer]] /; n == Length[order] :> Thread[ConstantArray[QuantumLabelName[subLabel], n] -> order],
+	Superscript[subLabel_, CircleTimes[n_Integer]] :> QuantumLabelName[subLabel, order],
+	Subscript["R", subLabel_Composition][angle_] :> ({"R", Sow[Chop @ angle, #], QuantumLabelName[#]} -> order & /@ Reverse[List @@ subLabel]),
+	Subscript["R", subLabel_][angle_] :> {"R", Sow[Chop @ angle, subLabel], QuantumLabelName[subLabel, order]},
+	"\[Pi]"[perm__] :> {"Permutation", PermutationCycles[{perm}]},
+	name_ :> If[order === {}, name, name -> order]
 }]
 
