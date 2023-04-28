aa1425aea62f1dc666999e2d51af785975836ef6
Nikolay Murzin <nikm@wolfram.com>
2023-04-28 11:02:05 +0300

    tweak QuantumEvolve

--- QuantumFramework/Kernel/QuantumEvolution.m
+++ QuantumFramework/Kernel/QuantumEvolution.m
@@ -4,6 +4,8 @@ PackageExport["QuantumEvolve"]
 
 
 
+QuantumEvolve::error = "Differential Solver failed to find a solution"
+
 QuantumEvolve[
     hamiltonian_ ? QuantumOperatorQ,
     defaultState : _ ? QuantumStateQ | Automatic : Automatic,
@@ -13,7 +15,8 @@ QuantumEvolve[
     state = Replace[defaultState, Automatic :> QuantumState[{"Register", hamiltonian["InputDimensions"]}]],
     parameter,
     parameterSpec,
-    numericQ
+    numericQ,
+    solution
 },
     ConfirmAssert[state["OutputDimensions"] == hamiltonian["InputDimensions"]];
     If[ defaultParameter === Automatic,
@@ -24,21 +27,31 @@ QuantumEvolve[
         parameterSpec = Replace[defaultParameter, _Symbol :> {parameter, 0, 1}]
     ];
     numericQ = MatchQ[defaultParameter, {_Symbol, _ ? NumericQ, _ ? NumericQ}];
-    QuantumState[
-        If[numericQ, NDSolveValue, DSolveValue][
-            {
-                \[FormalS]'[parameter] == 1 / I TrigToExp[hamiltonian["Matrix"]] . \[FormalS][parameter],
-                \[FormalS][0] == If[numericQ, Normal, Identity] @ state["State"]
-            },
-            If[ numericQ,
-                \[FormalS][parameterSpec[[-1]]],
-                Element[\[FormalS][parameter], If[state["VectorQ"], Vectors[state["Dimension"]], Matrices[state["MatrixDimensions"]]]]
-            ],
-            If[numericQ, parameterSpec, parameter],
-            args
+    solution = If[numericQ, NDSolveValue, DSolveValue][
+        {
+            \[FormalS]'[parameter] == 1 / I If[numericQ, Normal, Identity] @ TrigToExp[hamiltonian["Matrix"]] . \[FormalS][parameter],
+            \[FormalS][0] == If[numericQ, Normal, Identity] @ state["State"]
+        },
+        If[ numericQ,
+            \[FormalS],
+            Element[\[FormalS][parameter], If[state["VectorQ"], Vectors[state["Dimension"]], Matrices[state["MatrixDimensions"]]]]
         ],
-        hamiltonian["OutputBasis"],
-        If[numericQ, {}, "ParameterSpec" -> parameterSpec]
+        If[numericQ, parameterSpec, parameter],
+        args
+    ];
+    If[ MatchQ[solution, _InterpolatingFunction],
+        solution = With[{data = ResourceFunction["InterpolatingFunctionData"][solution]},
+            Interpolation[Thread[{data["Grid"], #}], InterpolationOrder -> data["InterpolationOrder"]][parameter] & /@ Transpose[data["ValuesOnGrid"]]
+        ]
+    ];
+    If[ stateQ[solution],
+        QuantumState[
+            solution,
+            hamiltonian["OutputBasis"],
+            "ParameterSpec" -> parameterSpec
+        ],
+        Message[QuantumEvolve::error];
+        solution
     ]
 ]
 
