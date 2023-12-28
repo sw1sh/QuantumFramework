ce05daf27b9c4e5c982c32a83a34b6f42b9fde86
Nikolay Murzin <nikm@wolfram.com>
2023-12-28 23:41:37 +0300

    explicit target for named measurements

--- QuantumFramework/Kernel/QuantumMeasurementOperator/NamedMeasurementOperators.m
+++ QuantumFramework/Kernel/QuantumMeasurementOperator/NamedMeasurementOperators.m
@@ -7,22 +7,25 @@ PackageScope["$QuantumMeasurementOperatorNames"]
 $QuantumMeasurementOperatorNames = {"RandomHermitian", "WignerMICPOVM"}
 
 
-QuantumMeasurementOperator[{"RandomHermitian", args___}, opts___] := With[{
+QuantumMeasurementOperator[{"RandomHermitian", args___}, target : _ ? targetQ : {1}, opts___] := With[{
     basis = QuantumBasis[args]
 },
     QuantumMeasurementOperator[
         QuantumOperator[
             With[{m = RandomComplex[1 + I, {basis["Dimension"], basis["Dimension"]}]}, (m + ConjugateTranspose[m]) / 2],
+            target,
             basis
         ],
         opts
 ]
 ]
 
-QuantumMeasurementOperator[{"WignerMICPOVM", args___}, opts___] := Enclose @ FullSimplify @ QuantumMeasurementOperator[
-    QuantumMeasurementOperator[ConfirmBy[QuantumWignerMICPOVM[args], ArrayQ[#, 3] &]],
-    With[{basis = QuditBasis[{"WignerMIC", args}]},
-        QuantumBasis[QuantumTensorProduct[QuditBasis[basis["Names"]], QuditBasis[Sqrt[basis["Dimension"]]]], QuditBasis[Sqrt[basis["Dimension"]]]]
+QuantumMeasurementOperator[{"WignerMICPOVM", args___}, target : _ ? targetQ : {1}, opts___] := Enclose @ FullSimplify @ QuantumMeasurementOperator[
+    QuantumMeasurementOperator[
+        QuantumMeasurementOperator[ConfirmBy[QuantumWignerMICPOVM[args], ArrayQ[#, 3] &], target],
+        With[{basis = QuditBasis[{"WignerMIC", args}]},
+            QuantumBasis[QuantumTensorProduct[QuditBasis[basis["Names"]], QuditBasis[Sqrt[basis["Dimension"]]]], QuditBasis[Sqrt[basis["Dimension"]]]]
+        ]
     ],
     opts
 ]
