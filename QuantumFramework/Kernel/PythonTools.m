2f1b0f88382dda8cb10f3e7a5a18e51a0474f7a7
Nikolay Murzin <nikm@wolfram.com>
2024-01-13 13:14:53 +0300

    check asset paclet versions, require new Python tools

--- QuantumFramework/Kernel/PythonTools.m
+++ QuantumFramework/Kernel/PythonTools.m
@@ -11,11 +11,19 @@ $PythonPackages = {"wolframclient", "qiskit", "matplotlib", "pylatexenc", "qiski
 $PythonSession := SelectFirst[
     ExternalSessions["Python"],
     #["ID"] == "QuantumFramework" &,
-    StartExternalSession[{{"Python", "StandardErrorFunction" -> Null},
-        "ID" -> "QuantumFramework",
-        "Evaluator" -> <|"Dependencies" -> $PythonPackages, "PythonRuntime" -> "3.11"|>,
-        "SessionProlog" -> "import qiskit\nfrom qiskit import QuantumCircuit"
-    }]
+    With[{versions = Through[PacletFind["ExternalEvaluate"]["Version"]], required = "32.2"},
+        If[ AllTrue[versions, ResourceFunction["VersionOrder"][#, required] < 0 &], 
+            Failure["DependencyFailure", <|
+                "MessageTemplate" ->  "ExternalEvaluate paclet should be at least version `` (availabe in Wolfram Language 14), but only versions {``} are found", 
+                "MessageParameters" -> {required, StringRiffle[versions, ", "]}
+            |>],
+            StartExternalSession[{{"Python", "StandardErrorFunction" -> Null},
+                "ID" -> "QuantumFramework",
+                "Evaluator" -> <|"Dependencies" -> $PythonPackages, "PythonRuntime" -> "3.11"|>,
+                "SessionProlog" -> "import qiskit\nfrom qiskit import QuantumCircuit"
+            }]
+        ]
+    ]
 ]
 
 
