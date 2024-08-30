3d7b9c00bb1f33400b8b52e3014f28009930c036
Nikolay Murzin <nikm@wolfram.com>
2024-08-30 22:14:21 +0300

    change some QuantumDistance definitions

--- QuantumFramework/Kernel/QuantumDistance.m
+++ QuantumFramework/Kernel/QuantumDistance.m
@@ -12,20 +12,31 @@ $QuantumDistances = {"Fidelity", "RelativeEntropy", "RelativePurity", "Trace", "
 
 QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ] := QuantumDistance[qs1, qs2, "Fidelity"]
 
-QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "Fidelity"] := With[{
+QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "Fidelity"] /; qs1["Dimension"] == qs2["Dimension"] := With[{
     rootDensityMatrix = MatrixPower[qs1["Computational"]["DensityMatrix"], 1 / 2]
 },
     1 - Re[Tr[MatrixPower[rootDensityMatrix . qs2["Computational"]["DensityMatrix"] . rootDensityMatrix, 1 / 2]]]
 ]
 
-QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "RelativeEntropy"] := With[{
-    s = qs1["Computational"]["DensityMatrix"],
-    t = qs2["Computational"]["DensityMatrix"]
-},
-    Quantity[Chop[Tr[s . (MatrixLog[s, Method -> "Jordan"] - MatrixLog[t, Method -> "Jordan"])]] / Log[2], "Bits"]
+QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "RelativeEntropy"] /; qs1["Dimension"] == qs2["Dimension"] := If[
+    qs2["PureStateQ"],
+
+    qs1["Entropy"],
+
+    Block[{
+        s = qs1["Computational"]["DensityMatrix"],
+        t = qs2["Computational"]["DensityMatrix"],
+        slog, tlog, entropy, crossTerm
+    },
+        slog = MatrixLog[s, Method -> "Jordan"];
+        tlog = MatrixLog[t, Method -> "Jordan"];
+        entropy = - Tr[s . slog];
+        crossTerm = - Tr[s . tlog];
+        Quantity[Chop[If[Abs[crossTerm] <= 2 Log[qs1["Dimension"]], crossTerm - entropy, If[Chop[entropy] == 0, - Tr[t . tlog], Tr[t . tlog] - Tr[t . slog]]] / Log[2]], "Bits"]
+    ]
 ]
 
-QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "RelativePurity"] := With[{
+QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "RelativePurity"] /; qs1["Dimension"] == qs2["Dimension"] := With[{
     s = qs1["Computational"]["DensityMatrix"],
     t = qs2["Computational"]["DensityMatrix"]
 },
@@ -33,23 +44,23 @@ QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "RelativePurity"] :=
 ]
 
 
-QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "Trace"] := With[{
+QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "Trace"] /; qs1["Dimension"] == qs2["Dimension"] := With[{
     m = qs1["Computational"]["DensityMatrix"] - qs2["Computational"]["DensityMatrix"]
 },
     Re @ Tr[MatrixPower[ConjugateTranspose[m] . m, 1 / 2]] / 2
 ]
 
-QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "Bures"] :=
-    Sqrt[2 (1 - Sqrt @ QuantumDistance[qs1, qs2, "Fidelity"])]
+QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "Bures"] /; qs1["Dimension"] == qs2["Dimension"]  :=
+    Sqrt[2 (1 - QuantumDistance[qs1, qs2, "Fidelity"])]
 
-QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "BuresAngle"] :=
+QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "BuresAngle"] /; qs1["Dimension"] == qs2["Dimension"]  :=
     Re @ ArcCos @ QuantumDistance[qs1, qs2, "Fidelity"]
 
-QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "HilbertSchmidt"] :=
+QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "HilbertSchmidt"] /; qs1["Dimension"] == qs2["Dimension"] :=
     Sqrt @ Re @ Tr @ MatrixPower[qs1["Computational"]["DensityMatrix"] - qs2["Computational"]["DensityMatrix"], 2]
 
-QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "Bloch"] :=
-    Re @ EuclideanDistance[qs1["BlochCartesianCoordinates"], qs2["BlochCartesianCoordinates"]]
+QuantumDistance[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, "Bloch"] /; qs1["Dimension"] == qs2["Dimension"] :=
+    Re @ EuclideanDistance[qs1["BlochCartesianCoordinates"], qs2["BlochCartesianCoordinates"]] / 2
 
 
 QuantumSimilarity[qs1_ ? QuantumStateQ, qs2_ ? QuantumStateQ, distance_String : "Fidelity"] := 1 - QuantumDistance[qs1, qs2, distance]
